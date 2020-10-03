5dd67f6dcc8bd0b3f4e1ba28fe36e16b19e0f666
SUMIT KUMAR PANDIT <36043230+sumitkumarpandit@users.noreply.github.com>
2020-10-03 09:45:20 +0530

    Update gnt.java

--- Tree/Generic Tree/gnt.java
+++ Tree/Generic Tree/gnt.java
@@ -331,7 +331,7 @@ class rough{
      public static void main(String[]args) {
         int []ar={10,20,50,-1,-1,30,70,-1,80,110,-1,120,-1,-1,90,-1,-1,40,100,-1,-1,-1};
         construct(ar);
-        // display(root);
+        display(root);
         // System.out.println("\n-------\n");
         // System.out.println(maximum(root));
         // prepost(root);
